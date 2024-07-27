dcf01dfa8fe76928f219c893b6b68ec6b4fe5256
Fairmont77 <4ydovvans@gmail.com>
2024-07-27 18:16:01 +0300

    add config

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+CONFIG = {
+    "fuel": True, #set False if you dont want to auto update
+    "turbo": True, #set False if you dont want to auto update
+    "questions": {
+        "upgradeFuelTank": "Do you want to upgrade Fuel Tank? (y/n): ",
+        "upgradeTurboCharger": "Do you want to upgrade Turbo Charger? (y/n): "
+    }
+}
