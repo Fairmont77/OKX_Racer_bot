d06bd97330db8b59ca157aa3c860dda62c921640
Fairmont77 <4ydovvans@gmail.com>
2024-07-27 19:51:33 +0300

    v0.2

--- config.py
+++ config.py
@@ -1,9 +1,3 @@
-# config.py
-CONFIG = {
-    "fuel": True, #set False if you dont want to auto update
-    "turbo": True, #set False if you dont want to auto update
-    "questions": {
-        "upgradeFuelTank": "Do you want to upgrade Fuel Tank? (y/n): ",
-        "upgradeTurboCharger": "Do you want to upgrade Turbo Charger? (y/n): "
-    }
-}
+fuel = True
+turbo = True
+prediction = 0 # 0 - MOON || 1 - DOOM
